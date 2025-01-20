f570f61e1a01621c44719688afc0b7128fe785a1
Fawwaz <fawwaz-1427@hotmail.com>
2025-01-20 22:39:53 +0800

    Wrap Playground component in a div for improved layout and styling

--- src/app/writings/building-with-nextjs/playground.tsx
+++ src/app/writings/building-with-nextjs/playground.tsx
@@ -4,13 +4,15 @@ import { Sandpack } from '@codesandbox/sandpack-react';
 const Component = Sandpack as any;
 export default function Playground() {
   return (
-    <Component
-      template="react"
-      files={{
-        '/App.js': `export default function App() {
-  return <h1>Hello Sandpacfffk</h1>
-}`,
-      }}
-    />
+    <div className="lg:-mx-48 my-8">
+        <Component
+          template="react"
+          files={{
+            '/App.js': `export default function App() {
+          return <h1>Hello Sandpacfffk</h1>
+        }`,
+          }}
+        />
+    </div>
   );
 }
